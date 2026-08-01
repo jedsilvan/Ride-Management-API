de2e6931540286c71230f413fd5e41f67e286b38
Jed Silvan <jedsilvan@gmail.com>
2026-08-01 09:32:53 +0800

    feat: Init urls and wsgi

---
+++ wingz/wsgi.py
@@ -0,0 +1,10 @@
+"""
+WSGI config for the wingz project.
+"""
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wingz.settings")
+
+application = get_wsgi_application()
